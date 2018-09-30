6eed5afc04e4390658461452a26caca020068058
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-30 11:46:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
-def position_taken(board, index)
-
-end
+taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
